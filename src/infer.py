5dbdf99644a55cbf5b5a37c60616555c0bc9f8db
SrujanHrudhay <155370110+SrujanHrudhay@users.noreply.github.com>
2026-01-11 22:36:06 +0530

    Update infer.py

--- src/infer.py
+++ src/infer.py
@@ -1,1 +1,13 @@
-#inference
+from ultralytics import YOLO
+import argparse
+
+def main(args):
+    model = YOLO(args.weights)
+    model.predict(source=args.source, save=True)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--weights", required=True)
+    parser.add_argument("--source", required=True)
+    args = parser.parse_args()
+    main(args)
