5d0ececbe492222150f8b37075db4e796da55fe7
SrujanHrudhay <155370110+SrujanHrudhay@users.noreply.github.com>
2026-01-11 22:11:10 +0530

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,13 @@
 # Evaluation script
+from ultralytics import YOLO
+import argparse
+
+def main(args):
+    model = YOLO(args.weights)
+    model.val()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--weights", required=True)
+    args = parser.parse_args()
+    main(args)
